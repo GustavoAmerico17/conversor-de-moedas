5c2b8aee0743cd2249ab92cd5b7048eb147bb731
GustavoAmerico17 <gustavorodrigues.bsb@gmail.com>
2024-01-14 11:18:15 -0300

    atualizaçao do conversor

--- conversor de moedas/script.js
+++ conversor de moedas/script.js
@@ -1,9 +1,52 @@
-const convertButton = document.querySelector("#button-convert")
+    const convertButton = document.querySelector ("#button-convert")
+    const selectConvert = document.querySelector(".convert-select")
+    function botaoClick(){
+        const inputvalue = document.querySelector(".input-convert").value
+        const realValue = document.querySelector(".coin-value-real")
+        const dolarValue = document.querySelector(".coin-value-usa")
+    const dolarToday = 4.8548
+    const euroToday = 5.32
+    
 
-function botaoClick (){
-    console.log("deu bom")
+    if(selectConvert.value == "dolar"){
+    dolarValue.innerHTML= new Intl.NumberFormat("en-ES",{
+        style: "currency",
+        currency: "USD"
+    }).format (inputvalue / dolarToday)
+    }
+     
+    if(selectConvert.value == "euro"){
+        dolarValue.innerHTML= new Intl.NumberFormat("de-DE",{
+        style:"currency",
+        currency:"EUR"
+    }).format(inputvalue / euroToday)
+    }
+    realValue.innerHTML=new Intl.NumberFormat("pt-BR",{
+        style:"currency",
+        currency: "BRL"
+    }).format(inputvalue)
+    
 }
+    
+    function changeCurrency(){
+        const currencyName = document.getElementById ("currency-name")
+        const currencyImage = document.querySelector(".currency-img")
+        if(selectConvert.value == "dolar"){      
+        currencyName.innerHTML= "Dólar Americano"
+        currencyImage.src = "./logo Dolar.png"
+ 
+    }       
+        if(selectConvert.value == "euro"){
+            currencyName.innerHTML= "Euro"
+            currencyImage.src= "./logo Euro.png"
+        }
+        botaoClick()
+}    
 
 
 
-convertButton.addEventListener("click",botaoClick())
+
+
+
+selectConvert.addEventListener("change",changeCurrency);
+convertButton.addEventListener("click",botaoClick);
